c0986caeb5926b2a6a4e3aea3ab4a628582fd519
Ssuamje <side2850@naver.com>
2023-10-24 11:03:24 +0900

    invalid 한 맵 테스트하는 sh 작성

--- test_invalid_maps.sh
+++ test_invalid_maps.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Directory containing map files
 map_directory="./maps"
-
-# Find all files in the map directory that start with "invalid_"
 map_files=("$map_directory"/invalid_*)
 
-# Check if there are any matching files
 if [ ${#map_files[@]} -eq 0 ]; then
   echo "No map files starting with 'invalid_' found in $map_directory"
   exit 1
 fi
 
-# Loop through the matching files and execute ./cub3D with them
 for map_file in "${map_files[@]}"; do
-  echo "Running ./cub3D with file: $map_file"
+  echo "./cub3D $map_file"
   ./cub3D "$map_file"
+  echo ""
 done
